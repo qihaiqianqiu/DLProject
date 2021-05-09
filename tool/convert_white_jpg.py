9da2aa48679b636e3db51c837ef8d446973b5d68
qihaiqianqiu <44798852+qihaiqianqiu@users.noreply.github.com>
2021-05-09 14:35:46 +0800

    Fix resized image on a center position

--- tool/convert_white_jpg.py
+++ tool/convert_white_jpg.py
@@ -10,13 +10,15 @@ def convert_dataset(folder, new_folder):
         old_name = os.path.join(folder, name)
         new_name = os.path.join(new_folder, name)
 
-        img = Image.open(old_name)
-        x,y = img.size
-        try:
-          p = Image.new('RGB', img.size , (255,255,255))
-          p.paste(img, (0,0,x,y),img)
-          p.save(new_name.strip('.png') + '.jpg')
-        except:
-          pass
+        img = Image.open(old_name).convert("RGB")
+        img2 = Image.open(old_name).convert("RGBA")
+        x,y = img.size 
+        new_point = x / 256
+        new_x = int(256*1.05*new_point)
+        new_y = int(176*1.05*new_point)
+        p = Image.new('RGB', (new_x,new_y), (255,255,255))
+        p.paste(img, (int(0.05*new_x),int(0.03*new_y) , int(0.05*new_x)+x, int(0.03*new_y)+y), img2)
+        p.save(new_name.strip('.png') + '.jpg')
+
   
 convert_dataset(root_dir + '/raw', root_dir + '/test')
