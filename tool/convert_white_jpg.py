54d11a8a6cb7feb7d7ea48a455d027ebf622ac3a
qihaiqianqiu <44798852+qihaiqianqiu@users.noreply.github.com>
2021-05-17 16:59:07 +0800

    Update convert_white_jpg.py

--- tool/convert_white_jpg.py
+++ tool/convert_white_jpg.py
@@ -1,56 +1,28 @@
-from skimage.io import imread, imsave
-from skimage.transform import resize
-import os
-import numpy as np
-import pandas as pd
-import json
 from PIL import Image
+import os
 import sys
 
-def resize_dataset(folder, new_folder, new_size = (256, 176), crop_bord=40):
+root_dir = '/content/DLProject/deepfashion'
+
+def convert_dataset(folder, new_folder):
     if not os.path.exists(new_folder):
         os.makedirs(new_folder)
     for name in os.listdir(folder):
         old_name = os.path.join(folder, name)
         new_name = os.path.join(new_folder, name)
 
+        img = Image.open(old_name).convert("RGB")
+        img2 = Image.open(old_name).convert("RGBA")
+        x,y = img.size 
+        new_point = 1.1*y / 256
+        new_x = int(176*new_point)
+        new_y = int(256*new_point)
+        p = Image.new('RGB', (new_x,new_y), (255,255,255))
+        #p = Image.new('RGB', img.size, (255,255,255))
+        p.paste(img, (int(new_x/2-x/2),int(new_y/2-y/2) , int(new_x/2-x/2)+x, int(new_y/2-y/2)+y), img2)
+        #p.paste(img,(0,0,x,y),img2)
+        p.save(new_name.strip('.png') + '.jpg')
 
-        img = Image.open(old_name)
-        w, h =img.size
-        if crop_bord == 0:
-            pass
-        else:
-            img = img.crop((crop_bord, 0, w-crop_bord, h))
-        img = img.resize([new_size[1],new_size[0]])
-        img.save(new_name)
-        print('resize %s succefully' % old_name)
+convert_dataset(root_dir + '/raw_' + sys.argv[1], root_dir + '/'+ sys.argv[1])
 
-def resize_annotations(name, new_name, new_size = (256, 176), old_size = (256, 256), crop_bord=40):
-    df = pd.read_csv(name, sep=':')
-
-    ratio_y = new_size[0] / float(old_size[0])
-    ratio_x = new_size[1] / float(old_size[1] - 2 * crop_bord)
-
-    def modify(values, ratio, crop):
-        val = np.array(json.loads(values))
-        mask = val == -1
-        val = ((val - crop) * ratio).astype(int)
-        val[mask] = -1
-        return str(list(val))
-
-    df['keypoints_y'] = df.apply(lambda row: modify(row['keypoints_y'], ratio_y, 0), axis=1)
-    df['keypoints_x'] = df.apply(lambda row: modify(row['keypoints_x'], ratio_x, crop_bord), axis=1)
-
-    df.to_csv(new_name, sep=':', index=False)
-
-
-root_dir = '/content/DLProject/deepfashion'
-
-#resize_dataset(root_dir + '/test', root_dir + '/fashion_resize/test')
-# resize_annotations(root_dir + 'fasion-annotation-test.csv', root_dir + 'fasion-resize-annotation-test.csv')
-
-#resize_dataset(root_dir + '/train', root_dir + '/fashion_resize/train')
-# resize_annotations(root_dir + 'fasion-annotation-train.csv', root_dir + 'fasion-resize-annotation-train.csv')
-
-resize_dataset(root_dir + '/' + sys.argv[1], root_dir + '/fashion_resize/' + sys.argv[1])
 
